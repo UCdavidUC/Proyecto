5e9107504feac4b26a5e1a49976fdf1986e2c381
David Sanchez <divad.dad@gmail.com>
2015-04-20 22:10:54 -0500

    Commit del nodo
    Creación del nodo y lectura de archivo de entrada.

--- src/segundaparte/Principal.java
+++ src/segundaparte/Principal.java
@@ -5,6 +5,9 @@
  */
 package segundaparte;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.Scanner;
 
 /**
@@ -14,7 +17,11 @@ import java.util.Scanner;
  * @version 1.0.0
  */
 public class Principal {
-    public static void main(String args[]) {
+    public static void main(String args[]) throws IOException {
         Scanner in = new Scanner(System.in);
+        String path = in.nextLine();
+        String archivo = '"' + path + '"';
+        FileReader fileReader = new FileReader(archivo);
+        BufferedReader br = new BufferedReader(fileReader);
     }
 }
